3bc3464b664afa1306e1f334d6f38d9cc137da28
moyotoh <206204062+moyotoh@users.noreply.github.com>
2026-04-16 12:43:50 +0200

    im a fucking idiot

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// 🔧 Replace these with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+    apiKey: "AIzaSyDW-HsndyJlo-A9wsD6nVk0NcLJbTPj9_A",
+  authDomain: "tore-barcode.firebaseapp.com",
+  projectId: "tore-barcode",
+  storageBucket: "tore-barcode.firebasestorage.app",
+  messagingSenderId: "238997857731",
+  appId: "1:238997857731:web:41ead174e0b5a49eecc976",
+  key: "AIzaSyDne70MvbSQfyrl95nFU-UjBslJcWDCsQY"
 };
 
 const app = initializeApp(firebaseConfig);
